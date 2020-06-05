ff72cd9f99fa346f77f646a538302d3943fcd218
李冠漢 <Dirk.Lee@want-want.com>
2020-06-05 17:37:10 +0800

    修改Job 確定執行字段再加上執行名稱.

--- src/views/Jobs/Job.vue
+++ src/views/Jobs/Job.vue
@@ -6,30 +6,23 @@
           <label>Description</label>
           <md-input v-model="dataJob.memo"></md-input>
         </md-field>
-        <md-chips
-          v-model="dataJob.expressions"
-          md-placeholder="ex: 秒 分 時 日 月 星期"
-        ></md-chips>
+        <md-chips v-model="dataJob.expressions" md-placeholder="ex: 秒 分 時 日 月 星期"></md-chips>
         <div class="block">
           <h3>Error Control</h3>
-          <md-radio v-model="dataJob.strategy" value="continues"
-            >Continue</md-radio
-          >
-          <md-radio v-model="dataJob.strategy" value="interrupt"
-            >Interrupt</md-radio
-          >
+          <md-radio v-model="dataJob.strategy" value="continues">Continue</md-radio>
+          <md-radio v-model="dataJob.strategy" value="interrupt">Interrupt</md-radio>
         </div>
         <div class="block">
           <h3>Dynamic</h3>
           <div class="inset-container" style="display: block; min-height: 116px; overflow: auto;">
-             <Dynamic
-                style="margin: 10px;"
-                v-for="(dynamic, index) in dataJob.dynamics"
-                :key="dynamic.key"
-                :index="index"
-                :current="dynamic"
-                @delete="deleteDynamic"
-              ></Dynamic>
+            <Dynamic
+              style="margin: 10px;"
+              v-for="(dynamic, index) in dataJob.dynamics"
+              :key="dynamic.key"
+              :index="index"
+              :current="dynamic"
+              @delete="deleteDynamic"
+            ></Dynamic>
             <md-speed-dial class="md-bottom-right">
               <md-button class="md-fab md-primary" @click="addDynamic">
                 <md-icon>add</md-icon>
@@ -70,8 +63,23 @@
         </div>
       </md-card-content>
       <md-card-actions>
-        <md-button class="md-raised md-primary" @click="save">Save</md-button>
+        <md-button class="md-raised md-primary" @click="confirmDialog.active = true">Save</md-button>
       </md-card-actions>
+
+      <md-dialog-confirm
+        :md-active.sync="confirmDialog.active"
+        md-title="確定要儲存?"
+        md-confirm-text="確定"
+        md-cancel-text="取消"
+        @md-cancel="confirmDialog.active = false"
+        @md-confirm="save"
+      />
+
+       <md-dialog-alert
+        :md-active.sync="alert.visible"
+        :md-content="alert.content"
+        md-confirm-text="Close"
+      />
     </md-card>
   </div>
 </template>
@@ -86,12 +94,12 @@ export default {
   components: {
     draggable,
     Dynamic,
-    Task,
+    Task
   },
   props: {
     id: {
-      type: String,
-    },
+      type: String
+    }
   },
   data() {
     return {
@@ -100,6 +108,13 @@ export default {
       dataJcoServerList: [],
       enabled: false,
       dragging: false,
+      confirmDialog: {
+        active: false
+      },
+      alert: {
+        content: "",
+        visible: false
+      }
     };
   },
   created() {
@@ -110,7 +125,7 @@ export default {
   watch: {
     id() {
       this.getJob();
-    },
+    }
   },
   methods: {
     getJob() {
@@ -118,13 +133,13 @@ export default {
         method: "post",
         url: "job/getJob",
         params: {
-          jobId: this.id,
+          jobId: this.id
         },
         headers: {
-          "Content-Type": "application/json",
-        },
+          "Content-Type": "application/json"
+        }
       })
-        .then((resp) => {
+        .then(resp => {
           const respData = resp.data;
           if (respData.returnCode === 1) {
             // 請求成功
@@ -132,18 +147,18 @@ export default {
             const results = respData.data;
 
             this.$nextTick(() => {
-              results.tasks.forEach((task) => {
+              results.tasks.forEach(task => {
                 task.key = uuid.v4();
               });
               this.dataJob = { ...results };
             });
           } else {
             // 請求失敗
-            this.alertContent = "取得紀錄詳情失敗";
-            this.alertVisible = true;
+            this.alert.content = "取得紀錄詳情失敗";
+            this.alert.visible = true;
           }
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
@@ -152,10 +167,10 @@ export default {
         method: "post",
         url: "setting/getJDBCServers",
         headers: {
-          "Content-Type": "application/json",
-        },
+          "Content-Type": "application/json"
+        }
       })
-        .then((resp) => {
+        .then(resp => {
           const respData = resp.data;
           if (respData.returnCode === 1) {
             // 請求成功
@@ -166,11 +181,11 @@ export default {
             });
           } else {
             // 請求失敗
-            this.alertContent = "取得紀錄詳情失敗";
-            this.alertVisible = true;
+            this.alert.content = "取得紀錄詳情失敗";
+            this.alert.visible = true;
           }
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
@@ -179,10 +194,10 @@ export default {
         method: "post",
         url: "setting/getJCOServers",
         headers: {
-          "Content-Type": "application/json",
-        },
+          "Content-Type": "application/json"
+        }
       })
-        .then((resp) => {
+        .then(resp => {
           const respData = resp.data;
           if (respData.returnCode === 1) {
             // 請求成功
@@ -194,18 +209,18 @@ export default {
             });
           } else {
             // 請求失敗
-            this.alertContent = "取得紀錄詳情失敗";
-            this.alertVisible = true;
+            this.alert.content = "取得紀錄詳情失敗";
+            this.alert.visible = true;
           }
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
     addDynamic: function() {
       this.dataJob.dynamics.push({ key: uuid.v4() });
     },
-    deleteDynamic: function(dynamic) { 
+    deleteDynamic: function(dynamic) {
       this.dataJob.dynamics.splice(this.dataJob.dynamics.indexOf(dynamic), 1);
     },
     addTask: function() {
@@ -219,29 +234,31 @@ export default {
         method: "post",
         url: "job/saveJob",
         data: JSON.stringify({
-          ...this.dataJob,
+          ...this.dataJob
         }),
         headers: {
-          "Content-Type": "application/json",
-        },
+          "Content-Type": "application/json"
+        }
       })
-        .then((resp) => {
+        .then(resp => {
           const respData = resp.data;
           if (respData.returnCode === 1) {
             // 請求成功
             // console.log("respData.data.content = ", respData.data);
             const results = respData.data;
+            this.alert.content = "儲存成功";
+            this.alert.visible = true;
           } else {
             // 請求失敗
-            this.alertContent = "取得紀錄詳情失敗";
-            this.alertVisible = true;
+            this.alert.content = "取得紀錄詳情失敗";
+            this.alert.visible = true;
           }
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
-    },
-  },
+    }
+  }
 };
 </script>
 
